cc31c60f6ec871c3937024c5716ea55260e63178
Ahmad Mukhtar <ahmadmukhtar2001@gmail.com>
2026-06-13 13:48:22 +0500

    Distorted names of players for copy right avoidance

--- lib/data/teams_data.dart
+++ lib/data/teams_data.dart
@@ -8,92 +8,92 @@ const Team brazil = Team(
   name: 'Brazil',
   countryCode: 'BR',
   shooters: [
-    Player(name: 'Vinicius Jr', power: 80, accuracy: 75, curve: 82),
-    Player(name: 'Rodrygo', power: 72, accuracy: 78, curve: 70),
-    Player(name: 'Raphinha', power: 70, accuracy: 72, curve: 85),
-    Player(name: 'Paquetá', power: 75, accuracy: 82, curve: 65),
-    Player(name: 'Endrick', power: 88, accuracy: 70, curve: 60),
+    Player(name: 'Vinny Junior', power: 80, accuracy: 75, curve: 82),
+    Player(name: 'Rodryggo Goes', power: 72, accuracy: 78, curve: 70),
+    Player(name: 'Raffhinha', power: 70, accuracy: 72, curve: 85),
+    Player(name: 'Lukas Paketa', power: 75, accuracy: 82, curve: 65),
+    Player(name: 'Endrik', power: 88, accuracy: 70, curve: 60),
   ],
-  keeper: GoalkeeperRating(name: 'Alisson', reflex: 90, prediction: 88),
+  keeper: GoalkeeperRating(name: 'Allisson Bekker', reflex: 90, prediction: 88),
 );
 
 const Team argentina = Team(
   name: 'Argentina',
   countryCode: 'AR',
   shooters: [
-    Player(name: 'Lionel Messi', power: 82, accuracy: 96, curve: 95),
-    Player(name: 'Lautaro Martínez', power: 85, accuracy: 87, curve: 65),
-    Player(name: 'Julián Álvarez', power: 81, accuracy: 85, curve: 72),
-    Player(name: 'Alexis Mac Allister', power: 80, accuracy: 84, curve: 81),
-    Player(name: 'Alejandro Garnacho', power: 76, accuracy: 75, curve: 78),
+    Player(name: 'Leon Mesi', power: 82, accuracy: 96, curve: 95),
+    Player(name: 'Lautro Martynz', power: 85, accuracy: 87, curve: 65),
+    Player(name: 'Julien Alvarz', power: 81, accuracy: 85, curve: 72),
+    Player(name: 'Alexi McAlester', power: 80, accuracy: 84, curve: 81),
+    Player(name: 'Alejo Garnachio', power: 76, accuracy: 75, curve: 78),
   ],
-  keeper: GoalkeeperRating(name: 'Emi Martínez', reflex: 91, prediction: 92),
+  keeper: GoalkeeperRating(name: 'Emilio Martynz', reflex: 91, prediction: 92),
 );
 
 const Team portugal = Team(
   name: 'Portugal',
   countryCode: 'PT',
   shooters: [
-    Player(name: 'Cristiano Ronaldo', power: 91, accuracy: 89, curve: 76),
-    Player(name: 'Bruno Fernandes', power: 87, accuracy: 86, curve: 85),
-    Player(name: 'Bernardo Silva', power: 68, accuracy: 90, curve: 86),
-    Player(name: 'Rafael Leão', power: 81, accuracy: 76, curve: 73),
-    Player(name: 'João Félix', power: 75, accuracy: 81, curve: 84),
+    Player(name: 'Cristian Rinaldi', power: 91, accuracy: 89, curve: 76),
+    Player(name: 'Bruno Fernancio', power: 87, accuracy: 86, curve: 85),
+    Player(name: 'Bernardo Sylva', power: 68, accuracy: 90, curve: 86),
+    Player(name: 'Rafa Leo', power: 81, accuracy: 76, curve: 73),
+    Player(name: 'Joao Feliks', power: 75, accuracy: 81, curve: 84),
   ],
-  keeper: GoalkeeperRating(name: 'Diogo Costa', reflex: 88, prediction: 86),
+  keeper: GoalkeeperRating(name: 'Diogo Kostas', reflex: 88, prediction: 86),
 );
 
 const Team france = Team(
   name: 'France',
   countryCode: 'FR',
   shooters: [
-    Player(name: 'Kylian Mbappé', power: 89, accuracy: 90, curve: 79),
-    Player(name: 'Antoine Griezmann', power: 77, accuracy: 88, curve: 86),
-    Player(name: 'Ousmane Dembélé', power: 75, accuracy: 74, curve: 81),
-    Player(name: 'Marcus Thuram', power: 83, accuracy: 80, curve: 64),
-    Player(name: 'Bradley Barcola', power: 72, accuracy: 78, curve: 77),
+    Player(name: 'Killian M\'Bape', power: 89, accuracy: 90, curve: 79),
+    Player(name: 'Antwan Griezman', power: 77, accuracy: 88, curve: 86),
+    Player(name: 'Usman Dembely', power: 75, accuracy: 74, curve: 81),
+    Player(name: 'Mark Thuram', power: 83, accuracy: 80, curve: 64),
+    Player(name: 'Bradley Barcla', power: 72, accuracy: 78, curve: 77),
   ],
-  keeper: GoalkeeperRating(name: 'Mike Maignan', reflex: 90, prediction: 89),
+  keeper: GoalkeeperRating(name: 'Mike Maygnan', reflex: 90, prediction: 89),
 );
 
 const Team england = Team(
   name: 'England',
   countryCode: 'GB',
   shooters: [
-    Player(name: 'Harry Kane', power: 91, accuracy: 94, curve: 75),
-    Player(name: 'Jude Bellingham', power: 83, accuracy: 87, curve: 74),
-    Player(name: 'Bukayo Saka', power: 77, accuracy: 85, curve: 82),
-    Player(name: 'Phil Foden', power: 80, accuracy: 89, curve: 84),
-    Player(name: 'Cole Palmer', power: 82, accuracy: 90, curve: 87),
+    Player(name: 'Harry Kaine', power: 91, accuracy: 94, curve: 75),
+    Player(name: 'Jude Bellinghem', power: 83, accuracy: 87, curve: 74),
+    Player(name: 'Bukayo Sakka', power: 77, accuracy: 85, curve: 82),
+    Player(name: 'Phillip Fodden', power: 80, accuracy: 89, curve: 84),
+    Player(name: 'Cole Palmier', power: 82, accuracy: 90, curve: 87),
   ],
-  keeper: GoalkeeperRating(name: 'Jordan Pickford', reflex: 86, prediction: 85),
+  keeper: GoalkeeperRating(name: 'Jordon Pickfort', reflex: 86, prediction: 85),
 );
 
 const Team spain = Team(
   name: 'Spain',
   countryCode: 'ES',
   shooters: [
-    Player(name: 'Lamine Yamal', power: 74, accuracy: 85, curve: 90),
-    Player(name: 'Nico Williams', power: 77, accuracy: 79, curve: 76),
-    Player(name: 'Álvaro Morata', power: 80, accuracy: 82, curve: 62),
-    Player(name: 'Dani Olmo', power: 78, accuracy: 86, curve: 82),
-    Player(name: 'Pedri', power: 69, accuracy: 87, curve: 81),
+    Player(name: 'Lamine Jamal', power: 74, accuracy: 85, curve: 90),
+    Player(name: 'Nico Williamz', power: 77, accuracy: 79, curve: 76),
+    Player(name: 'Alvaro Morato', power: 80, accuracy: 82, curve: 62),
+    Player(name: 'Daniel Olmon', power: 78, accuracy: 86, curve: 82),
+    Player(name: 'Pedro Gonzales', power: 69, accuracy: 87, curve: 81),
   ],
-  keeper: GoalkeeperRating(name: 'Unai Simón', reflex: 87, prediction: 88),
+  keeper: GoalkeeperRating(name: 'Unai Symon', reflex: 87, prediction: 88),
 );
 
 const Team germany = Team(
   name: 'Germany',
   countryCode: 'DE',
   shooters: [
-    Player(name: 'Florian Wirtz', power: 76, accuracy: 88, curve: 85),
-    Player(name: 'Jamal Musiala', power: 73, accuracy: 87, curve: 83),
-    Player(name: 'Kai Havertz', power: 79, accuracy: 84, curve: 71),
-    Player(name: 'Niclas Füllkrug', power: 88, accuracy: 83, curve: 59),
-    Player(name: 'Leroy Sané', power: 83, accuracy: 77, curve: 82),
+    Player(name: 'Florian Virts', power: 76, accuracy: 88, curve: 85),
+    Player(name: 'Jamal Musialah', power: 73, accuracy: 87, curve: 83),
+    Player(name: 'Kai Havretz', power: 79, accuracy: 84, curve: 71),
+    Player(name: 'Niklas Fullkrug', power: 88, accuracy: 83, curve: 59),
+    Player(name: 'Leroy San', power: 83, accuracy: 77, curve: 82),
   ],
   keeper:
-      GoalkeeperRating(name: 'Marc-André ter Stegen', reflex: 89, prediction: 88),
+      GoalkeeperRating(name: 'Mark-Andre Ter Steegen', reflex: 89, prediction: 88),
 );
 
 const Team pakistan = Team(
